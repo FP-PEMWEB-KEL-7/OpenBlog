fcfa7ba07bcfcdf67514f09a4820458263115823
rmffo <19081010050@upnjatim.ac.id>
2021-12-29 12:08:38 +0700

    wip logged_in_home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,6 +6,11 @@ class Home extends CI_Controller {
 	public function index()
 	{
 		$this->load->helper('url'); 
+
+		if ($this->session->userdata('logged_in')){
+			return $this->logged_in();
+		}
+
 		$this->load->model('Artikel');
 		$data['title'] = 'Open Blog | Home';
 		$data['artikels'] = $this->Artikel->get_limit(3);
@@ -17,6 +22,24 @@ class Home extends CI_Controller {
 		}
 
 		$this->load->view('home', $data);
+	}
+
+	public function logged_in()
+	{
+		$this->load->helper('url'); 
+		$this->load->model('Artikel');
+		$data['title'] = 'Open Blog | Home';
+		$akun = $this->session->userdata('user');
+
+		$data['artikels'] = $this->Artikel->get_by_author($akun->name);
+		
+		for ($i=0; $i < count($data['artikels']); $i++) { 
+			$data['artikels'][$i]->content = substr($data['artikels'][$i]->content, 0, 200);
+			$data['artikels'][$i]->title = substr($data['artikels'][$i]->title, 0, 30);
+			$data['artikels'][$i]->author_name = substr($data['artikels'][$i]->author_name, 0, 30);
+		}
+
+		$this->load->view('home_logged_in', $data);
 	}
 
 }
---
+++ application/views/home_logged_in.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title><?= $title; ?></title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="<?php echo base_url(); ?>assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="<?php echo base_url(); ?>css/styles.css" rel="stylesheet" />
+    </head>
+    <body class="d-flex flex-column h-100">
+        <main class="flex-shrink-0">
+            <!-- Navigation-->
+            <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+                <div class="container px-5">
+                    <a class="navbar-brand" href="index_login.html"><img src="<?php echo base_url(); ?>assets/medium-logo.png" style="width : 150px"; alt=""></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item"><a class="nav-link" href="write.html"><img src="<?php echo base_url(); ?>assets/plus.png" style="width:20px"></a></li>
+                            <li><div class="dropdown">
+                                <button class="btn" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <div class="">
+                                        <img class="rounded-circle" width="30px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg">
+                                      </div>
+                                </button>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                  <li><a class="dropdown-item" href="setting.html">Settings</a></li>
+                                  <hr>
+                                  <li><a class="dropdown-item text-danger" href="logout">Log out</a></li>
+                                </ul>
+                              </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            
+            
+            <hr class= "mx-auto" style="border: 1px black; width:800px; align : center;">
+            <!-- Blog preview section-->
+            <section class="py-5">
+                <div class="container px-5 my-2">
+                    <div class="row gx-5 justify-content-center">
+                        <div class="col-lg-8 col-xl-6">
+                            <div class="text-center">
+                                <h2 class="fw-bolder">Find Your Article</h2>
+                                <br>
+                                <br>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row gx-5">
+						<?php
+							foreach ($artikels as $artikel) {
+						?>
+                        <div class="col-lg-4 mb-5">
+                            <div class="card h-100 shadow border-0">
+                                <div class="card-body p-4">
+                                    <a class="text-decoration-none link-dark stretched-link" href="post_login.html"><h5 class="card-title mb-3"><?= $artikel->title; ?></h5></a>
+                                    <p class="card-text mb-0"><?= $artikel->content; ?></p>
+                                </div>
+                                <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
+                                    <div class="d-flex align-items-end justify-content-between">
+                                        <div class="d-flex align-items-center">
+                                            <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." />
+                                            <div class="small">
+                                                <div class="fw-bold"><?= $artikel->author_name; ?></div>
+                                                <div class="text-muted"><?= $artikel->createdAt; ?></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+						<?php
+							}
+						?>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <!-- Footer-->
+        <footer class="bg-dark py-4 mt-auto">
+            <div class="container px-5">
+                <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                    <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; Open Blog 2021</div></div>
+                </div>
+            </div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="<?php echo base_url(); ?>js/scripts.js"></script>
+    </body>
+</html>
