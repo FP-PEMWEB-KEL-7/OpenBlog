2f8f7423fabbca66689bbc9dbf791495a400d7e7
rmffo <19081010050@student.upnjatim.ac.id>
2021-12-30 06:16:01 +0700

    fix delete controller

--- application/controllers/Delete.php
+++ application/controllers/Delete.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Edit extends CI_Controller {
+class Delete extends CI_Controller {
 
 	public function index($id = null)
 	{
