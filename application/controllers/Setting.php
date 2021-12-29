c442ce8780621508fefccb54ae5ebe97d9950452
rmffo <19081010050@upnjatim.ac.id>
2021-12-29 13:40:45 +0700

    wip setting

--- application/controllers/Setting.php
+++ application/controllers/Setting.php
@@ -51,6 +51,8 @@ class Setting extends CI_Controller {
 			return $this->load->view('setting', $data);
 		}
 
+		$akun = $this->Akun->get('id', $data['akun']->id, true);
+		$this->session->set_userdata('user', $akun[0]);
 		$this->load->view('setting', $data);
 	}
 
