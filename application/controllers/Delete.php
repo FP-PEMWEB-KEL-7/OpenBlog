7a5b8812da6f995e59554e8717443ba852cc26e2
rmffo <19081010050@student.upnjatim.ac.id>
2021-12-30 06:24:07 +0700

    delete post

--- application/controllers/Delete.php
+++ application/controllers/Delete.php
@@ -30,17 +30,16 @@ class Delete extends CI_Controller {
 		$this->load->view('delete', $data);
 	}
 
-	public function post($id = null)
+	public function proceed($id = null)
 	{
 		$this->load->helper('url');
-		$this->session->set_flashdata('message', '');
 
 		if (!$this->session->userdata('logged_in')){
 			return redirect('/');
 		}
 
         $this->load->model('Artikel');
-		$data['title'] = "Open Blog | Edit Post";
+		$data['title'] = "Open Blog | Delete Post";
 		$data['akun'] = $this->session->userdata('user');
 
 		$data['artikel'] = $this->Artikel->get_by_id($id);
@@ -55,38 +54,8 @@ class Delete extends CI_Controller {
 			return redirect('/');
 		}
 
-		$this->form_validation->set_rules('title', 'Title', 'required');
-		$this->form_validation->set_rules('cerita', 'Cerita', 'required');
-
-		if ($this->form_validation->run() == false) {
-			return $this->load->view('edit', $data);
-		}
-
-        $gambar_link = $this->input->post('gambar_link');
-		$title = $this->input->post('title');
-		$cerita = $this->input->post('cerita');
-
-		if (empty($data['akun'])) {
-			$this->session->set_flashdata('message', 'Akun tidak ditemukan');
-			return $this->load->view('edit', $data);
-		}
-
-		$artikel = $this->Artikel->update($id, [
-            'gambar_link' => $gambar_link,
-			'title' => $title,
-			'content' => $cerita,
-            'author' => $data['akun']->id
-		]);
-
-		if (!$artikel) {
-			$this->session->set_flashdata('message', 'Gagal mengubah artikel');
-			return $this->load->view('edit', $data);
-		}
-
-		$data['artikel'] = $this->Artikel->get_by_id($id);
-		$data['artikel'] = $data['artikel'][0];
-        $this->session->set_flashdata('message', 'Berhasil mengubah artikel');
-		$this->load->view('edit', $data);
+		$this->Artikel->remove($id);
+		redirect('/');
 	}
 
 }
