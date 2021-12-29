989e96917ad3795192d0d8f573912d42c1406fe1
micjef <michaeljeffrysetiawan@gmail.com>
2021-12-29 23:21:11 +0700

    update home login screen

--- application/views/home_logged_in.php
+++ application/views/home_logged_in.php
@@ -62,7 +62,7 @@
                         <div class="col-lg-4 mb-5">
                             <div class="card h-100 shadow border-0">
                                 <div class="card-body p-4">
-                                    <a class="text-decoration-none link-dark stretched-link" href="post_login.html"><h5 class="card-title mb-3"><?= $artikel->title; ?></h5></a>
+                                    <a class="text-decoration-none link-dark stretched-link" href="<?php echo "post/".$artikel->id; ?>"><h5 class="card-title mb-3"><?= $artikel->title; ?></h5></a>
                                     <p class="card-text mb-0"><?= $artikel->content; ?></p>
                                 </div>
                                 <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
